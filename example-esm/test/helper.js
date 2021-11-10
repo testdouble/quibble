f810a4c851ca561f9ead8eae85fd2dd2d66e4927
Yagiz Nizipli <yagiz@nizipli.com>
2021-11-10 11:57:06 -0500

    feat: add support for json modules

--- example-esm/test/helper.js
+++ example-esm/test/helper.js
@@ -4,6 +4,7 @@ const quibble = require('quibble')
 beforeEach(function () {
   quibble.esm('../lib/animals/bear.mjs', undefined, function () { return 'a fake bear' })
   quibble.esm('../lib/animals/lion.mjs', undefined, function () { return 'a fake lion' })
+  quibble.esm('../lib/foo.json', undefined, { 'hello': 'quibble' })
 })
 
 afterEach(function () {
--- example-esm/test/lib/zoo-spec.mjs
+++ example-esm/test/lib/zoo-spec.mjs
@@ -5,9 +5,11 @@ const { expect } = chai
 
 describe('zoo', function () {
   var subject
+  var json
 
   beforeEach(async function () {
     subject = await import('../../lib/zoo.mjs')
+    json = await import('../../lib/foo.json')
   })
 
   it('contains a fake bear', function () {
@@ -16,5 +18,9 @@ describe('zoo', function () {
 
   it('contains a fake lion', function () {
     expect(subject.default().animals).to.contain('a fake lion')
+  })
+
+  it('manipulates json', function () {
+    expect(json.default).to.deep.equal({ hello: 'quibble' })
   })
 })
