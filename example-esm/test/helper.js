25e6f9af2b17346eda18f8fa05dfe997e4bc1def
Adrien Dorsaz <adrien.dorsaz@winbiz.ch>
2023-02-07 14:26:38 +0100

    fix example-esm: it needs to use the quibble.esm

--- example-esm/test/helper.js
+++ example-esm/test/helper.js
@@ -1,9 +1,9 @@
 const { beforeEach, afterEach } = require('mocha')
 const quibble = require('quibble')
 
-beforeEach(function () {
-  quibble('../lib/animals/bear.mjs', undefined, function () { return 'a fake bear' })
-  quibble('../lib/animals/lion.mjs', undefined, function () { return 'a fake lion' })
+beforeEach(async function () {
+  await quibble.esm('../lib/animals/bear.mjs', undefined, function () { return 'a fake bear' })
+  await quibble.esm('../lib/animals/lion.mjs', { default: function () { return 'a fake lion' }})
 })
 
 afterEach(function () {
