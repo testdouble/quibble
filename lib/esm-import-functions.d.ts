65e6b1b42cb37ecd60181c93ea4f0e0ca2bb8290
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2024-05-31 09:58:53 -0300

    refactor: use a single typings file

--- lib/esm-import-functions.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function dummyImportModuleToGetAtPath(
-  modulePath: string
-): Promise<string>;
-
-export declare function importOriginalModule(
-  fullImportPath: string
-): Promise<any>;
--- lib/quibble-registered.d.mts
+++
@@ -1,5 +0,0 @@
-import { GlobalPreloadOptions } from './quibble.mjs';
-
-export * from './quibble.mjs';
-
-export declare function initialize({ port }: GlobalPreloadOptions): void;
