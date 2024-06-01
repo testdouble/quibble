fac450e65d68a00411a7ed51fc29f19af581f95b
Weslley Araújo <46850407+wellwelwel@users.noreply.github.com>
2024-05-31 22:18:13 -0300

    Introduce initial set of typescript types for quibble

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+export type QuibbleConfig = {
+  defaultFakeCreator: (request: string) => any;
+};
+
+declare const quibble: ((request: string, stub?: any) => any) & {
+  config(userConfig: Partial<QuibbleConfig>): QuibbleConfig;
+  ignoreCallsFromThisFile(file?: string): void;
+  reset(hard?: boolean): void;
+  absolutify(relativePath: string, parentFileName?: string): string;
+  esm(
+    specifier: string,
+    namedExportStubs?: Record<string, any>,
+    defaultExportStub?: any
+  ): Promise<void>;
+  listMockedModules(): string[];
+  isLoaderLoaded(): boolean;
+  esmImportWithPath(specifier: string): Promise<{
+    modulePath: string;
+    moduleUrl: string;
+    module: any;
+  }>;
+};
+
+export default quibble;
